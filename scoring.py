26b0e9a0dfe36178aef90be3e6c52a9d92436fda
Marc Brinkmann <git@marcbrinkmann.de>
2011-02-20 23:33:37 +0100

    Added 2-player elo scoring.

--- scoring.py
+++ scoring.py
@@ -32,3 +32,59 @@ class TallyScoring(object):
 					break
 
 		return player_scores
+
+
+class EloDict(defaultdict):
+	def __init__(self, initial_average, *args, **kwargs):
+		self.initial_average = initial_average
+		super(EloDict, self).__init__(None, *args, **kwargs)
+
+	def __missing__(self, player):
+		if not self: return self.initial_average
+		# calculate current average and return it
+		return int(round(sum(self.values())/float(len(self))))
+
+
+class EloScoring(object):
+	def __init__(self, k_factors = { None: 32, 2100: 24, 2401: 12 }, initial_average = 1000, initial_scores = {}):
+		self.k_factors = sorted(k_factors.iteritems()) # none gets sorted to the front, always
+		self.initial_average = initial_average
+		self.initial_scores = initial_scores
+
+	def calculate_scores(self, games):
+		points = EloDict(self.initial_average, )
+		points.update(self.initial_scores)
+
+		for game in games:
+			(player_a, gamescore_a), (player_b, gamescore_b) = game.iteritems()
+
+			# calculate result
+			if gamescore_a > gamescore_b: result = 1.0
+			elif gamescore_a < gamescore_b: result = 0.0
+			else: result = 0.5
+
+			# update points
+			points[player_a], points[player_b] = self.calculate_single_match_score(points[player_a], points[player_b], result)
+
+		return points
+
+
+	def calculate_single_match_score(self, points_a, points_b, result):
+		"""calculate the new elo rankings for a single match. points_a and points_b are the
+		   players current ratings, result is 1.0 if player a won, 0.0 if player b won
+		   and 0.5 on a draw"""
+		expected = 1./(1+10**((points_b-points_a)/400.))
+
+		# note: float conversion implicit, since expected is always a float
+		return (int(round(points_a + self.get_k_factor_for(points_a) * (result - expected))),
+		        int(round(points_b + self.get_k_factor_for(points_b) * (expected - result))))
+
+	def get_k_factor_for(self, points):
+		"""get the k factor for a player that has a certain amount of points"""
+		# for a *lot* of k values this should be a search tree, but i really don't
+		# expect that to be a common use case...
+		for boundary, k_factor in self.k_factors:
+			if boundary > points: break
+			k = k_factor
+
+		return k
--- tests/test_scoring.py
+++ tests/test_scoring.py
@@ -57,3 +57,70 @@ class TestTallyScoring(unittest.TestCase):
 
 		scores = scoring_system.calculate_scores(games)
 		self.assertDictEqual(expected_scores, dict(scores))
+
+
+class TestEloDict(unittest.TestCase):
+	def test_elo_dict_initial_average(self):
+		e = EloDict(1000)
+
+		self.assertEqual(e['x'], 1000)
+		self.assertEqual(e['y'], 1000)
+
+	def test_elo_dict_average(self):
+		e = EloDict(1000)
+		e['a'] = 3
+		e['b'] = 7
+
+		self.assertEqual(e['c'], 5)
+
+	def test_elo_dict_average_stays_same(self):
+		e = EloDict(1000)
+
+		e['a'] = 3
+		e['b'] = 7
+
+		self.assertEqual(e['c'], 5)
+		self.assertEqual(e['d'], 5)
+
+		e['c'] = 5
+		self.assertEqual(e['d'], 5)
+
+
+class TestEloScoringStatic(unittest.TestCase):
+	def setUp(self):
+		# example from http://de.wikipedia.org/wiki/Elo-Zahl
+		self.kasparow = u'Garri Kasparov'
+		self.polgar = u'Zsuzsa Polgár'
+
+		elos = {
+			self.kasparow: 2806,
+			self.polgar: 2577,
+		}
+
+		k_factors = {None: 10}
+
+		self.points = EloScoring(k_factors = k_factors, initial_scores = elos)
+
+	def test_elo_polgar_win(self):
+		expected = {
+			self.kasparow: 2798,
+			self.polgar: 2585,
+		}
+		result = self.points.calculate_scores([{self.kasparow: -999, self.polgar: 1123}])
+		self.assertDictEqual(expected, result)
+
+	def test_elo_kasparov_win(self):
+		expected = {
+			self.kasparow: 2808,
+			self.polgar: 2575,
+		}
+		result = self.points.calculate_scores([{self.kasparow: 13999, self.polgar: 13998}])
+		self.assertDictEqual(expected, result)
+
+	def test_elo_draw_game(self):
+		expected = {
+			self.kasparow: 2803,
+			self.polgar: 2580,
+		}
+		result = self.points.calculate_scores([{self.kasparow: -5.0, self.polgar: -5.0}])
+		self.assertDictEqual(expected, result)
